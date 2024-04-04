9530d1b12b283f1e432a60f07114268f6da354c9
Manoj Kumar LT <161209500+manugadari@users.noreply.github.com>
2024-04-04 10:19:44 +0530

    Create main.tf
    nanbgs

---
+++ main.tf
@@ -0,0 +1,24 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.16"
+    }
+  }
+
+  required_version = ">= 1.2.0"
+}
+
+provider "aws" {
+  region     = "us-west-2"
+  access_key = "AKIAX75DMDWK76O3NIMV"
+  secret_key = "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"
+}
+resource "aws_instance" "Manojkumar-LT" {
+  ami           = "ami-830c94e3"
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "Terraform_Demo"
+  }
+}
